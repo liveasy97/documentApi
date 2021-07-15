5f46603f0bbde2762303b1568c7b81c4444a9ea7
ADMIN <vivek.bhosale098@gmail.com>
2021-07-15 18:06:25 +0530

    env file test

--- DocumentAPI/src/main/java/com/springboot/DocumentAPI/Config/AWSS3Client.java
+++ DocumentAPI/src/main/java/com/springboot/DocumentAPI/Config/AWSS3Client.java
@@ -11,22 +11,26 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Configuration
 public class AWSS3Client {
+	
+	Dotenv dotenv = Dotenv.load();
 
 	@Value("${cloud.aws.region.static}")
 	private String region;
 	
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
+	//@Value("${cloud.aws.credentials.access-key}")
+	//private String accessKey;
 	
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String secretKey;
+	//@Value("${cloud.aws.credentials.secret-key}")
+	//private String secretKey;
 	
 	@Bean
 	@Primary
 	public AmazonS3 s3client() {
-		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		AWSCredentials credentials = new BasicAWSCredentials(dotenv.get("ACCESS_KEY"), dotenv.get("SECRET_KEY"));
 		return AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(credentials))
 				.withRegion(region)
