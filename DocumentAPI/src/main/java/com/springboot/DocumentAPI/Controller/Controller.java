3fd7e0523a956fc069a78f642dd8bec2ca607744
Vivek Bhosale <69669689+vivekbhosale@users.noreply.github.com>
2021-07-16 12:32:55 +0530

    Update Controller.java

--- DocumentAPI/src/main/java/com/springboot/DocumentAPI/Controller/Controller.java
+++ DocumentAPI/src/main/java/com/springboot/DocumentAPI/Controller/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
 	
 	@GetMapping("/home")
 	public String home() {
-		return "Welcome to documentApi git action check with env file...!!!";
+		return "Welcome to documentApi git action with env file check 2...!!!";
 	}
 	
 	@PostMapping("/document")
